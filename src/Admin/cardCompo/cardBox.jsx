d004518726effd2e7b6ee99ca246fe86e5b27b5b
Binu_Tamang <bunutmg323@gmail.com>
2024-11-24 22:12:51 +0545

    feat(admin): add new admin components and styles
    - Added new AdminHeader.jsx and AdminLayout.jsx components.
    - Added new CSS files for admin components (dmin.css, cardBox.css, login.css).
    - Created new cardBox.jsx and login.jsx components for admin functionality.
    - Updated App.jsx to integrate new components.
    - Added logo image (logo.webp) to assets.

---
+++ src/Admin/cardCompo/cardBox.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./cardBox.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+const CardBox = () => {
+  return (
+    <div className="card">
+      
+    </div>
+  );
+};
+
+export default CardBox;
